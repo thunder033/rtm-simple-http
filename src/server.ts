9ce1185f31259ada8914d4cab51b8a57d127c539
Greg <gjr8050@rit.edu>
2017-02-01 11:00:55 -0500

    us tslint

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+/// <reference path="../node_modules/@types/node/index.d.ts" />
+
+import * as Http from 'http';
+/**
+ * Created by gjrwcs on 1/31/2017.
+ */
+const port: number = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (req: Http.IncomingMessage, response: Http.ServerResponse) => {
+    console.log(req.url);
+};
+
+Http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0101:${port}`);
